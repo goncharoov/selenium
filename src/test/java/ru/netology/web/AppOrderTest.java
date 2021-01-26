b1af4f4f67a0b039e1880a3cb45e85f3e5759e5d
goncharoov <takeyo69@icloud.com>
2021-01-26 18:02:59 +1000

    fix

--- src/test/java/ru/netology/web/AppOrderTest.java
+++ src/test/java/ru/netology/web/AppOrderTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,13 +17,12 @@ public class AppOrderTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
         driver = new ChromeDriver();
     }
 
